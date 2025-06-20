16ad3c6285f05c0611637ecea710efac3af6ead5
Anton <anton.tuhadiansyah@gmail.com>
2025-06-20 12:25:59 +0900

    code: remove debug

--- src/components/QrReader.jsx
+++ src/components/QrReader.jsx
@@ -61,7 +61,6 @@ const QrReader = ({ onScan }) => {
 
   return (
     <div className="qr-reader">
-      <div className="debug-label">QR SCANNER DEBUG</div>
       <video ref={videoEl}></video>
       <div ref={qrBoxEl} className="qr-box">
         {cameraReady && (
